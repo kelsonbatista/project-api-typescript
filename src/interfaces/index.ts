4a06afcc4000f99672ad4a8389aae1097c01a948
kelsonbatista <kelsonbatista@gmail.com>
2022-06-10 16:51:49 -0300

    feat(interfaces): created interfaces index file

---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+import { IProduct } from './productInterface';
+import { IOrder } from './orderInterface';
+import { IUser } from './userInterface';
+import { ErrorHandler } from './errorInterface';
+
+export { IProduct, IOrder, IUser, ErrorHandler };
