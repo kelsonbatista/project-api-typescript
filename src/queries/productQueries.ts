91e33869f7fd161f52cdcf32a1042a3494e41261
kelsonbatista <kelsonbatista@gmail.com>
2022-06-11 01:07:27 -0300

    fix(product queries): removed orderId from create and update queries

--- src/queries/productQueries.ts
+++ src/queries/productQueries.ts
@@ -1,7 +1,7 @@
 export default {
   getAllProducts: 'SELECT * FROM Trybesmith.Products',
   getProductById: 'SELECT * FROM Trybesmith.Products WHERE id = ?',
-  createProduct: 'INSERT INTO Trybesmith.Products (name, amount, orderId) VALUES (?, ?, ?)',
-  updateProduct: 'UPDATE Trybesmith.Products SET name = ?, amount = ?, orderId = ? WHERE id = ?',
+  createProduct: 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+  updateProduct: 'UPDATE Trybesmith.Products SET name = ?, amount = ? WHERE id = ?',
   removeProduct: 'DELETE FROM Trybesmith.Products WHERE id = ?',
 };
