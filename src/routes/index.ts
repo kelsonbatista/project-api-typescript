97cf4494cd73c109170090ca525b8d0bbe5f5aac
kelsonbatista <kelsonbatista@gmail.com>
2022-06-11 12:21:51 -0300

    feat(user routes): created user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import productRouter from './productRouter';
+import userRouter from './userRouter';
 
 const router = Router();
 
 router.use('/products', productRouter);
+
+router.use('/users', userRouter);
 
 export default router;
---
+++ src/routes/userRouter.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import UserController from '../controllers/userController';
+// import validateUser from '../middlewares/validateUserMiddleware';
+
+const userRouter = express.Router();
+
+const userController = new UserController();
+
+userRouter.get('/', userController.getAllUsers);
+
+userRouter.get('/:id', userController.getUserById);
+
+userRouter.post('/', userController.createUser);
+
+userRouter.put('/:id', userController.updateUser);
+
+userRouter.delete('/:id', userController.deleteUser);
+
+export default userRouter;
