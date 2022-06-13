721fdb06a47caa05f09c2cb19417fb303b1da2a8
kelsonbatista <kelsonbatista@gmail.com>
2022-06-13 13:41:00 -0300

    feat(queries): added getUserByUsername query

--- src/queries/userQueries.ts
+++ src/queries/userQueries.ts
@@ -1,6 +1,7 @@
 export default {
   getAllUsers: 'SELECT * FROM Trybesmith.Users',
   getUserById: 'SELECT * FROM Trybesmith.Users WHERE id = ?',
+  getUserByUsername: 'SELECT * FROM Trybesmith.Users WHERE username = ?',
   createUser: 'INSERT INTO Trybesmith.Users (username,classe,level,password) VALUES (?, ?, ?, ?)',
   updateUser: 'UPDATE Trybesmith.Users SET username=?, classe=?, level=?, password=? WHERE id=?',
   deleteUser: 'DELETE FROM Trybesmith.Users WHERE id = ?',
