c319e3015638496246aea4164c933d5d381d7e4a
kelsonbatista <kelsonbatista@gmail.com>
2022-06-14 12:05:10 -0300

    refactor(order queries): all order queries edited

--- src/queries/orderQueries.ts
+++ src/queries/orderQueries.ts
@@ -1,7 +1,10 @@
 export default {
-  getAll: 'SELECT * FROM Trybesmith.Orders',
-  getById: 'SELECT * FROM Trybesmith.Orders WHERE id = ?',
-  create: 'INSERT INTO Trybesmith.Orders (userId) VALUES (?, ?)',
-  update: 'UPDATE Trybesmith.Orders SET name = ?, amount = ?, orderId = ? WHERE id = ?',
-  remove: 'DELETE FROM Trybesmith.Orders WHERE id = ?',
+  getAllOrders: `SELECT ord.id, ord.userId, prd.id AS productsIds
+                FROM Trybesmith.Orders AS ord
+                JOIN Trybesmith.Products AS prd
+                ON ord.id = prd.orderId`,
+  getOrderById: 'SELECT * FROM Trybesmith.Orders WHERE id = ?',
+  createOrder: 'INSERT INTO Trybesmith.Orders (id, userId) VALUES (?, ?)',
+  updateOrder: 'UPDATE Trybesmith.Orders SET userId = ? WHERE id = ?',
+  deleteOrder: 'DELETE FROM Trybesmith.Orders WHERE id = ?',
 };
