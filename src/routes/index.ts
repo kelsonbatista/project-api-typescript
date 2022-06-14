8668dd3cdf51a96c39ded4a2ffa3b15e3bcf049c
kelsonbatista <kelsonbatista@gmail.com>
2022-06-14 12:07:36 -0300

    feat(order router): added main order router file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import productRouter from './productRouter';
 import userRouter from './userRouter';
+import orderRouter from './orderRouter';
 
 const router = Router();
 
 router.use('/products', productRouter);
 
 router.use('/users', userRouter);
+
+router.use('/orders', orderRouter);
 
 export default router;
