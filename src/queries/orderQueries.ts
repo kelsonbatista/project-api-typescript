3a510e8a85fd0442891ef8ab25ccd352cd69b14a
kelsonbatista <kelsonbatista@gmail.com>
2022-06-17 01:21:35 -0300

    feat(order queries): added createOrder and updateProductOrder queries

--- src/queries/orderQueries.ts
+++ src/queries/orderQueries.ts
@@ -4,7 +4,8 @@ export default {
                 JOIN Trybesmith.Products AS prd
                 ON ord.id = prd.orderId`,
   getOrderById: 'SELECT * FROM Trybesmith.Orders WHERE id = ?',
-  createOrder: 'INSERT INTO Trybesmith.Orders (id, userId) VALUES (?, ?)',
+  createOrder: 'INSERT INTO Trybesmith.Orders (userId) VALUES (?)',
   updateOrder: 'UPDATE Trybesmith.Orders SET userId = ? WHERE id = ?',
+  updateProductOrder: 'UPDATE Trybesmith.Products SET orderId = ? WHERE id = ?',
   deleteOrder: 'DELETE FROM Trybesmith.Orders WHERE id = ?',
 };
