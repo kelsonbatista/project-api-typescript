1b85d02a03568659bd6a70ce4f097bcab47698c6
kelsonbatista <kelsonbatista@gmail.com>
2022-06-17 01:17:52 -0300

    feat(order schema): added orderSchema path to schema index

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,9 +1,11 @@
 import productSchema from './productSchema';
 import userSchema from './userSchema';
 import loginSchema from './loginSchema';
+import orderSchema from './orderSchema';
 
 export {
   productSchema,
   userSchema,
   loginSchema,
+  orderSchema,
 };
