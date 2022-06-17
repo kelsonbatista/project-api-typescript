b6eb9f56f88c266e242ae545b103f84c7c281ebf
kelsonbatista <kelsonbatista@gmail.com>
2022-06-17 01:16:15 -0300

    feat(order router): added createOrder route with authToken and validateOrder

--- src/routes/orderRouter.ts
+++ src/routes/orderRouter.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import OrderController from '../controllers/orderController';
+import authToken from '../middlewares/authTokenMiddleware';
+import validateOrder from '../middlewares/validateOrderMiddleware';
 
 const orderRouter = express.Router();
 
 const orderController = new OrderController();
 
 orderRouter.get('/', orderController.getAllOrders);
+
+orderRouter.post('/', authToken, validateOrder, orderController.createOrder);
 
 export default orderRouter;
